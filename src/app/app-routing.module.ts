bbb7925162c5b3f84c7c6640b1ff6a99e167da1f
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-06-17 04:41:31 +0200

    Remove the unnecessary import in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -12,7 +12,7 @@ const routes: Routes = [
     component: RecipesComponent,
     children: [
       { path: '', component: RecipeStartComponent },
-      { path: ':id', component: RecipeItemComponent }
+      { path: ':id', component: RecipeDetailComponent }
     ]
   },
   { path: 'shopping-list', component: ShoppingListComponent }
