9867b26db1e0a27470d11d91f19f9e14ffa1d312
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-06-29 13:51:02 +0200

    Add method to add item

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,17 +9,17 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrl: './shopping-edit.component.css'
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef!: ElementRef;
-  @ViewChild('amountInput') amountInputRef!: ElementRef;
+  @ViewChild('shoppingListForm') shoppingForm!: NgForm;
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit(): void {}
 
   onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+    console.log(this.shoppingForm.value);
+    const ingredientName = this.shoppingForm.value.name;
+    const ingredientAmount = this.shoppingForm.value.amount;
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
     this.slService.addIngredient(newIngredient);
   }
 }
