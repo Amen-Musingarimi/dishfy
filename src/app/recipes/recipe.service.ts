36d3880903c89ea6ce3a191aac6cd7f3e400f8ef
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-07-22 16:27:40 +0200

    Fix the recipe ingredients potential bug

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,20 +8,7 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test recipe',
-      'https://www.southernliving.com/thmb/jM1YjcVqzkt-Ej6pMp7qK--c_9Q=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Millionaire_Spaghetti_019-34e9c04b1ae8405088f53450a048e413.jpg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
-    ),
-    new Recipe(
-      'Burger',
-      'A nice burger',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/12/Big-double-cheeseburger-f437c9c.jpg?quality=90&webp=true&resize=300,272',
-      [new Ingredient('Burger Row', 2), new Ingredient('Meat', 4)]
-    )
-  ];
+  private recipes: Recipe[] = [];
 
   constructor(private slServive: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +27,16 @@ export class DataStorageService {
     this.http
       .get<Recipe[]>(
         'https://dishfy-b2fd8-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        })
       )
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
